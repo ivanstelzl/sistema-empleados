76c7a2da233e9c2002c5bf5bc9853bbc2a538e20
Ivan <ivanstelzl@gmail.com>
2024-04-10 12:06:04 -0300

    Creo Intancia Flask

---
+++ src/ap.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+mysql = MySQL()
+
+
+if __name__ == "main":
+    app.run(debug=True)
+
+    
