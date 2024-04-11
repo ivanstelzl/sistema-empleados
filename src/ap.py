c8fdea934cc6b27d6256bb592e598c403992747a
Ivan <ivanstelzl@gmail.com>
2024-04-11 10:48:51 -0300

    conexion a mysql

--- src/ap.py
+++ src/ap.py
@@ -1,12 +1,38 @@
-from flask import Flask
-from flask import render_template
-from flaskext.mysql import MySQL
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+
 
 app = Flask(__name__)
-mysql = MySQL()
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Ivojubese7523'
+app.config['MYSQL_DB'] = 'empleados'
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-if __name__ == "main":
-    app.run(debug=True)
+@app.route('/add_contact', methods=['POST'])
+def add_contact():
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        print(fullname)
+        print(phone)
+        print(email)
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO empleados (nombre, correo, foto) VALUES (%s, %s, %s)', 
+                    (fullname, phone, email))
+        mysql.connection.commit()
+
+        return "receeived"
+    
+
+if __name__ == '__main__':
+    app.run(port=3000, debug = True)
+
 
     
